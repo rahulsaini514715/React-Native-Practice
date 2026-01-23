952af12bba756f67547f859137d720cc08f652d8
Rahul Saini <140973573+rahulsaini514715@users.noreply.github.com>
2026-01-23 21:52:31 +0530

    Get Data From Redux Store

--- componentsRedux/Redux_Lec_2/Header.js
+++ componentsRedux/Redux_Lec_2/Header.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
 } from 'react-native';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const cartData = useSelector((state)=>state.reducer)
+  const [cartItem, setCartItem] = useState(0);
+  useEffect(()=>{
+     setCartItem(cartData.length)
+  },[cartData])
   return (
     <View >
       <Text style={{ fontSize: 30,textAlign:"right",padding:10,backgroundColor:"orange" }}>
-        0
+        {cartItem}
       </Text>
     </View>
   );
--- componentsRedux/Redux_Lec_2/redux/reducer.js
+++ componentsRedux/Redux_Lec_2/redux/reducer.js
@@ -1,1 +1,17 @@
 import {ADD_TO_CART} from "./constants"
+
+
+
+const initialState = [];
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return [
+        ...state,
+        action.data
+      ];
+    default:
+      return state;
+  }
+};
